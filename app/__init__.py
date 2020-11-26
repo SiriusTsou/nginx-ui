9dbaa56b80321843a1e56d4ea263f8029eca3047
SiriusTsou <sirius.tsou@gmail.com>
2020-11-27 00:51:33 +0800

    add APPLICATION_ROOT to solve nginx proxy_pass relative path

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 from config import config
 from flask_moment import Moment
-
+import os
 
 moment = Moment()
 
@@ -13,10 +14,11 @@ def create_app(config_name):
     config[config_name].init_app(app)
     moment.init_app(app)
 
+    prefix = os.getenv('APPLICATION_ROOT') or '/'
     from app.ui import ui as ui_blueprint
-    app.register_blueprint(ui_blueprint)
+    app.register_blueprint(ui_blueprint, url_prefix=prefix)
 
     from app.api import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api')
+    app.register_blueprint(api_blueprint, url_prefix=prefix+'/api')
 
     return app
